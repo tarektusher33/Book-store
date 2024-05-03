5a988537c4b27a8f530f21e189d06026fb7a642b
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-03 15:02:02 +0600

    Initial set up

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor (){
+    console.log("hello World");
+  }
+}
