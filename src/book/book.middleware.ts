e3a6f67aaaf43a9ec1b8c8e2588ae0e0961cf1d6
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-03 19:28:15 +0600

    modify book mmiddleware

--- src/book/book.middleware.ts
+++ src/book/book.middleware.ts
@@ -4,7 +4,12 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class BookMiddleWare implements NestMiddleware {
   use(req: Request , res: Response, next: NextFunction) {
-    console.log("This is class based middleware implemented for book module");
+    let protocol = req.protocol;
+    let host = req.get("host");
+    let url = req.originalUrl;
+    let method = req.method;
+    let date = new Date().toDateString();
+    console.log(protocol +"://" + host + url + " " + method+ " "+ date);
     next();
   }
 }
