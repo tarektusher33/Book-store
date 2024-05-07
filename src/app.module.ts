eb156c389440852d219b77dd6c5e42cf7505cea6
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-07 14:47:05 +0600

    .env file : added environment variable file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { BookModule } from './book/book.module';
 import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BookModule, UserModule, AuthModule],
+  imports: [BookModule, UserModule, AuthModule, ConfigModule.forRoot({
+    isGlobal : true,
+    envFilePath : ".local.env",
+    // envFilePath : ".prod.env"
+
+  })],
   controllers: [AppController],
   providers: [],
 })
